14df00fea95c42a7f2d86dd182b69a06d373716f
Davi Ribeiro <davi.rb@hotmail.com>
2023-04-01 15:58:05 -0300

    Nova branch criada

--- main.js
+++ main.js
@@ -5,24 +5,29 @@ function validaForm(campoA, campoB) {
     const snumero = campoB;
     console.log("Valor do campo A: ", +pnumero)
     console.log("Valor do Campo B: ", +snumero)
-    return pnumero > snumero;
+    return pnumero >= snumero;
     
 }
 
 form.addEventListener('submit', function(e){
+    let formEValido = false;
+
     e.preventDefault();
     const primeiroNumero = document.getElementById('value1');
     const segundoNumero = document.getElementById('value2');
+    formEValido = validaForm(primeiroNumero.value, segundoNumero.value)
 
-    if (validaForm(primeiroNumero.value, segundoNumero.value)) {
+    if (formEValido) {
         console.log ("valor de A > B");
         alert("Formulário Não foi Aceito!")
+        
     
     } else{
         console.log("Valor de B > A")
         alert("Formulário Aceito!")
     }
-
+    primeiroNumero.value = '';
+    segundoNumero.value = '';
 
     console.log("valor de A", + primeiroNumero.value);
     console.log("valor de B", + segundoNumero.value);
