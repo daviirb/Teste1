3208de1acbd81c947389a5d19c1da6cc1e9181d3
Davi Ribeiro <davi.rb@hotmail.com>
2023-04-01 15:28:44 -0300

    JS corrigido

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
 const form = document.getElementById("form-validator");
 
-function validator(numeros){
-    const campoA = getElementById('campoA');
-    const campoB = getElementById('campoB');
+function validaForm(campoA, campoB) {
+    const pnumero = campoA;    
+    const snumero = campoB;
+    console.log("Valor do campo A: ", +pnumero)
+    console.log("Valor do Campo B: ", +snumero)
+    return pnumero > snumero;
     
 }
-    
-
 
 form.addEventListener('submit', function(e){
-
     e.preventDefault();
-    if(!campoA>campoB){
-        alert("A é maior que B");
-    } else {
-        alert("B é maior que A")
+    const primeiroNumero = document.getElementById('value1');
+    const segundoNumero = document.getElementById('value2');
+
+    if (validaForm(primeiroNumero.value, segundoNumero.value)) {
+        console.log ("valor de A > B");
+        alert("Formulário Não foi Aceito!")
+    
+    } else{
+        console.log("Valor de B > A")
+        alert("Formulário Aceito!")
     }
 
+
+    console.log("valor de A", + primeiroNumero.value);
+    console.log("valor de B", + segundoNumero.value);
 })
 
-console.log(form)
+
+console.log(form);
